86221c415cf21abb08ce8749569cec1a10108d6d
Fozat Kardouh <fozatkardouh@gmail.com>
2018-03-24 10:51:59 +0100

    done level 1

---
+++ encoder/src/main/java/at/refugeescode/encoder/endpoint/Encoder.java
@@ -0,0 +1,35 @@
+package at.refugeescode.encoder.endpoint;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
+
+import java.util.stream.Collectors;
+
+@RequestMapping
+@RestController
+public class Encoder {
+
+    private final String url = "http://localhost:9052/coin";
+    @Autowired
+    private RestTemplate restTemplate;
+
+    @PostMapping("/encode")
+    public String encode(@RequestBody String plaintext) {
+        return plaintext.chars().boxed()
+                .map(i -> (char) i.intValue())
+                .map(Object::toString)
+                .map(this::sentToMoris)
+                .collect(Collectors.joining());
+    }
+
+    private String sentToMoris(String s) {
+        ResponseEntity<String> response = restTemplate.postForEntity(url, s, String.class);
+        return response.getBody();
+    }
+
+}
